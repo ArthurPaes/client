ef416d2d4562b5a83f470769eb2eed5f506f7897
arthur <arthur_pls_@hotmail.com>
2020-08-01 12:13:14 -0300

    Functional

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,10 +1,78 @@
-import React, {useState, useEffect} from 'react'
-import queryString from 'query-string'
-import io from 'socket.io-client'
+import React, {useState, useEffect, useReducer} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import './Chat.css';
 
-const Chat =()=>{
+import InfoBar from '../InfoBar/InfoBar'
+import Input from '../Input/Input'
+import Messages from '../Messages/Messages'
+
+
+
+let socket;
+
+
+
+
+
+const Chat =({location})=>{
+
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+
+
+  const ENDPOINT = 'localhost:5000'
+
+
+
+  useEffect(()=>{
+    const {name, room} =  queryString.parse(location.search);
+
+    socket = io(ENDPOINT) //pass endpoint to the server
+
+    setName(name)
+    setRoom(room)
+
+    socket.emit('join', {name, room}, ()=>{
+
+    })
+
+    return ()=>{          //used for unmounting - simply how useEffect works? - hapening on the unmounting of the component
+      socket.emit('disconnect') //leaving the channel
+
+      socket.off()
+    }
+
+  }, [ENDPOINT, location.search])
+
+  useEffect(()=>{
+    socket.on('message', (message)=>{
+      setMessages([...messages, message])
+    })
+  },[messages])
+
+  //function for sending messages
+  const sendMessage = (event)=>{
+    event.preventDefault();
+
+    if(message){
+      socket.emit('sendMessage',message, ()=> setMessage(''))
+    }
+  }
+
+  console.log(message,messages)
+
   return (
-    <h1>Chat</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room}/>
+        <Messages messages={messages} name={name}/>
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+import closeIcon from '../../icons/closeIcon.png'
+import onlineIcon from '../../icons/onlineIcon.png'
+
+import './InfoBar.css'
+
+const InfoBar = ({room})=>{
+  return (
+
+    <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon}  alt="onlineImage"/>
+      <h3>{room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="closeimage" /></a>
+      </div>
+    </div>
+
+  )
+}
+
+export default InfoBar
